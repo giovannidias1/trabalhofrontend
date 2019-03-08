1fc8997f038a8d3840b3473d440aceabc52a99d7
Unknown <giovannifdias@gmail.com>
2019-03-08 16:58:21 -0300

    OS todos campos criados

--- src/app/rental/rental.component.ts
+++ src/app/rental/rental.component.ts
@@ -16,6 +16,81 @@ export class RentalComponent implements OnInit {
   itemsAgency: any;
   minDate: Date;
   clients: any;
+  bsValue = new Date();
+  brandItems: any = [
+    {
+      id: 1,
+      name: 'LG'
+    },
+    {
+      id: 2,
+      name: 'Brastemp'
+    },
+    {
+      id: 3,
+      name: 'Consul'
+    },
+    {
+      id: 4,
+      name: 'Samsung'
+    },
+    {
+      id: 5,
+      name: 'Electrolux'
+    },
+    {
+      id: 6,
+      name: 'Lofra'
+    },
+    {
+      id: 7,
+      name: 'Colormaq'
+    },
+    {
+      id: 8,
+      name: 'Dako'
+    },
+    {
+      id: 9,
+      name: 'Viking'
+    },
+    {
+      id: 10,
+      name: 'Tecno'
+    },
+    {
+      id: 11,
+      name: 'Libherr'
+    },
+    {
+      id: 12,
+      name: 'Elica'
+    },
+    {
+      id: 13,
+      name: 'Bertazzoni'
+    },
+    {
+      id: 14,
+      name: 'kitchenAid'
+    },
+    {
+      id: 15,
+      name: 'Gorenje'
+    },
+    {
+      id: 16,
+      name: 'Atlas'
+    },
+    {
+      id: 17,
+      name: 'Esmaltec'
+    },
+    {
+      id: 18,
+      name: 'Outra'
+    }
+  ];
 
   constructor(
     public restService: RestService,
